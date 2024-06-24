a7cdab88eb4a914894a2342445640287e8a52019
Iván González <cafeteru.dev@gmail.com>
2024-06-24 11:22:22 +0200

    Create project

---
+++ src/main/java/io/github/cafeteru/dialoguesOfFantasy/DialoguesOfFantasyApplication.java
@@ -0,0 +1,13 @@
+package io.github.cafeteru.dialoguesOfFantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialoguesOfFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialoguesOfFantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/cafeteru/dialoguesOfFantasy/DialoguesOfFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cafeteru.dialoguesOfFantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialoguesOfFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
