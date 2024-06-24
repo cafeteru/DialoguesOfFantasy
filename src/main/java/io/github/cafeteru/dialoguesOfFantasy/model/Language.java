841253d9c1d6ffc294abc845beca77e94eed7c0e
Iván González <cafeteru.dev@gmail.com>
2024-06-24 11:43:25 +0200

    Create empty entities

---
+++ src/main/java/io/github/cafeteru/dialoguesOfFantasy/model/Language.java
@@ -0,0 +1,20 @@
+package io.github.cafeteru.dialoguesOfFantasy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+}
